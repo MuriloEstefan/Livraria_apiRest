c4686e18ce4dcd6c59afcd3c3fec870436dec53b
Murilo Estefano <muriloestefano19@gmail.com>
2026-03-06 17:07:17 -0300

    Projeto inicial da API de Livraria

---
+++ src/models/Livro.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const livroSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    titulo: {
+      type: String, 
+      required: [true, "O titulo do livro é obrigatório"]
+    },
+    autor: {type: mongoose.Schema.Types.ObjectId, 
+      ref: 'autores', 
+      required: [true, "O(a) autor(a) é obrigatório"]
+    },
+    editora: {
+      type: String, 
+      required: [true, "A editora é obrigatório"],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      validate: {
+        validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+        },
+        message: "O numero de páginas deve estar entre 10 e 5000. Valor fornecido {VALUE}"
+      }
+    }
+  }
+);
+
+const livros= mongoose.model('livros', livroSchema);
+
+export default livros;
